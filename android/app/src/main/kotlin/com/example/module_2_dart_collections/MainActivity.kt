7e5ad11ccae9cc3aeb7347b172aa07fa31036c7b
khatunajannat <khatunajannat35@gmail.com>
2026-06-09 00:33:50 +0600

    commit 1

---
+++ android/app/src/main/kotlin/com/example/module_2_dart_collections/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.module_2_dart_collections
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import "dart:io" ;
+void main(){
+int opt=0;
+const String appTitle= "Student Grader v1.0 ";
+
+final Set<String> availableSubjects = {"English" , "Math", "Science", "History"};
+List<int> scores = [];
+Map<String, dynamic> studentDetails= {
+
+  "name": { },
+  "Scores" : [],
+  "Subjects " : [availableSubjects],
+  "Bonus" : null,
+  "Comments " : null,
+};
+List<Map> students=[];
+
+do {
+
+
+  print('''
+========= $appTitle===========
+1. Add Student
+2. Record Score
+3. Add Bonus Points
+4. Add Comment
+5. View All Students
+6. View Report Card
+7. Class Summary
+8.Exit
+
+ print(" Choose an option: ");
+
+'''
+  );
+  switch(opt){
+    case "1" :
+      List.add();
+
+
+
+
+  }
+
+
+
+}while(
+
+);
+
+
+}
