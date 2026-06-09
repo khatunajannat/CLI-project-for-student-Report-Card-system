6797e2e0c317cb8ea856d63f3cbdd27c64fe359d
khatunajannat <khatunajannat35@gmail.com>
2026-06-09 17:57:43 +0600

    commit 2.2

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,21 @@
-import "dart:io" ;
-void main(){
-int opt=0;
-const String appTitle= "Student Grader v1.0 ";
+import "dart:io";
 
-final Set<String> availableSubjects = {"English" , "Math", "Science", "History"};
-List<int> scores = [];
-Map<String, dynamic> studentDetails= {
+void main() {
+  //int opt=1;
+  const String appTitle = "Student Grader v1.0 ";
 
-  "name": { },
-  "Scores" : [],
-  "Subjects" : [availableSubjects],
-  "Bonus" : null,
-  "Comments" : null,
-};
-List<Map<String, dynamic>> students=[];
+  final Set<String> availableSubjects = {
+    "English",
+    "Math",
+    "Science",
+    "History",
+  };
+  List<int> scores = [];
 
-do {
+  List<Map<String, dynamic>> students = [];
 
-
-  print('''
+  do {
+    print('''
 ========= $appTitle===========
 1. Add Student
 2. Record Score
@@ -31,22 +28,66 @@ do {
 
  print(" Choose an option: ");
 
-'''
-  );
-  switch(opt){
-    case "1" :
-      students.add(studentDetails);
+''');
+
+    int opt = int.parse(stdin.readLineSync()!);
+
+    switch (opt) {
+      case 1:
+        Map<String, dynamic> studentDetails = {
+          "name": {},
+          "Scores": [],
+          "Subjects": {...availableSubjects},
+          "Bonus": null,
+          "Comments": null,
+        };
+
+        print("Enter Student name: ");
+        var n = stdin.readLineSync();
+        studentDetails["name"] = n;
+        print("$n is added to the student list\n");
+
+        students.add(studentDetails);
+        print(studentDetails);
+
+
+
+      case 2:
+        for(int x=0; x<students.length; x++){
+         print(students[x]["name"]);
+
+        }
+        print("Pick a student: ");
+        String? p = stdin.readLineSync() ;
+        for( Map y in students) {
+          if (y["name"] == p) {
+            print(y["Subjects"]);
+
+            print("Enter the scores for $p");
+            for (String sub in y["Subjects"]) {
+              print("Enter Score for $sub :\n ");
+              int num = int.parse(stdin.readLineSync()!);
+
+              while (num > 0 && num < 100) {
+                y["Scores"].add(num);
+                break;
+              }
+
+            }
+            print("Scores are :  ");
+            print(y["Scores"]);
+          }
+
+        }
+
+
+      case 3:
 
 
 
 
-  }
-
-
-
-}while(
-
-);
-
-
+      case 8:
+        return;
+    }
+  } while (true);
 }
